d52dff07e31575c93ab10cdcf2206c774930c068
faysalcudi <faysal.1957@outlook.com>
2021-08-06 15:55:37 +0300

    mvc ile stok takip programı
    mvc yapısı ile oluşturduğumuz mağazaların ihtiyaçlarını karşılayacak stok takip programını oluşturduk

---
+++ MvcProje/MvcProje/Controllers/MusterıController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcProje.Models.Entity;
+
+namespace MvcProje.Controllers
+{
+    public class MusterıController : Controller
+    {
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        // GET: Musteri
+        public ActionResult Index()
+        {
+            var degerler = db.TBLMUSTERILER.ToList();
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public ActionResult YeniMusteri()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult YeniMusteri(TBLMUSTERILER p1)
+        {
+            db.TBLMUSTERILER.Add(p1);
+            db.SaveChanges();
+            return View();
+        }
+        public ActionResult Sil(int id)
+        {
+            var mstr = db.TBLMUSTERILER.Find(id);
+            db.TBLMUSTERILER.Remove(mstr);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult MusteriGetir(int id)
+        {
+            var mus = db.TBLMUSTERILER.Find(id);
+            return View("MusteriGetir", mus);
+        }
+
+        public ActionResult Guncelle(TBLMUSTERILER p1)
+        {
+            var musteri = db.TBLMUSTERILER.Find(p1.MUSTERIID);
+            musteri.MUSTERIAD = p1.MUSTERIAD;
+            musteri.MUSTERISOYAD = p1.MUSTERISOYAD;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+
+
+    }
+}
